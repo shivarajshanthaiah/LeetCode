f9537e3d6abe50902672da0da7841443428d7762
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-05-26 20:21:49 +0530

    Time: 7 ms (60.24%), Space: 4.6 MB (19.7%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    temp := &ListNode{}
+    current := temp
+    carry := 0
+
+    for l1 != nil || l2 != nil{
+        var x,y int
+        if l1 != nil{
+            x = l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil{
+            y = l2.Val
+            l2 = l2.Next
+        }
+        sum := x+y+carry
+        carry = sum/10
+        current.Next = &ListNode{Val: sum % 10}
+        current = current.Next
+    }
+    if carry > 0{
+        current.Next = &ListNode{Val: carry}
+    }
+    return temp.Next
+}
