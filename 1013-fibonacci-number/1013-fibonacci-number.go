ad4558ed724d4d387f67313c8dfa6360bff26d0f
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-05-28 18:53:20 +0530

    Time: 9 ms (30.34%), Space: 2.1 MB (32.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.go
+++ 1013-fibonacci-number/1013-fibonacci-number.go
@@ -1,6 +1,14 @@
 func fib(n int) int {
-	for n <= 1 {
-			return n
-		}
-		return fib(n-1) + fib(n-2)
+	if n < 0 {
+		return 0
 	}
+	if n == 0 || n == 1 {
+		return n
+	}
+
+	fib1 := fib(n - 1)
+	fib2 := fib(n - 2)
+
+	fibunacci := fib1 + fib2
+	return fibunacci
+}
