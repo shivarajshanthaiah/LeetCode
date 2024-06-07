6b3270c0c131ddbbb13ba646d33326ce1ac73fa6
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-07 18:05:15 +0530

    Time: 36 ms (73.46%), Space: 6.5 MB (92.59%) - LeetHub

--- 0561-array-partition/0561-array-partition.go
+++ 0561-array-partition/0561-array-partition.go
@@ -1,10 +1,8 @@
 func arrayPairSum(nums []int) int {
 	res := 0
 	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		if i == 0 || i%2 == 0 {
+	for i := 0; i < len(nums); i += 2 {
 			res += nums[i]
-		}
 	}
 	return res
 }
