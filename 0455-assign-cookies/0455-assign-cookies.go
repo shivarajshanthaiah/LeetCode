e1671132f4d27054e6ef55db69c9f3c60c2e394c
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-09 12:13:49 +0530

    Time: 20 ms (64.29%), Space: 6.6 MB (32.59%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.go
+++ 0455-assign-cookies/0455-assign-cookies.go
@@ -1,7 +1,8 @@
 func findContentChildren(g []int, s []int) int {
 	sort.Ints(g)
 	sort.Ints(s)
-	i, j, count := 0, 0, 0
+	i, j := 0, 0
+	count := 0
 
 	for i < len(g) && j < len(s) {
 		if s[j] >= g[i] {
