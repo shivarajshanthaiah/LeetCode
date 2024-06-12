4e190535c4e7f24feaec2a444ac3f56d1fa14e28
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-12 08:54:27 +0530

    Time: 0 ms (100%), Space: 2.3 MB (77.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.go
+++ 0075-sort-colors/0075-sort-colors.go
@@ -1,9 +1,13 @@
 func sortColors(nums []int) {
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums)-1; i++ {
+		minIndex := i
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
+			if nums[minIndex] > nums[j] {
+				minIndex = j
 			}
+		}
+		if minIndex != i {
+			nums[i], nums[minIndex] = nums[minIndex], nums[i]
 		}
 	}
 }
