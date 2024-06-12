d5ea508214784ce6ff3355dc194e79d63f8a1347
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-12 10:54:51 +0530

    Time: 19 ms (58.03%), Space: 7.3 MB (23.36%) - LeetHub

---
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	} else if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+    return nil
+}
