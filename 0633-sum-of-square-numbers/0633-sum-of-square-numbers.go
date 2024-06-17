f25a5ed86f3369dbb2659dc312219cb857750b2d
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-17 22:33:11 +0530

    Time: 0 ms (100%), Space: 2.2 MB (72.41%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.go
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.go
@@ -1,9 +1,8 @@
 func judgeSquareSum(c int) bool {
-	sum := 0
 	i := 0
     j := int(math.Sqrt(float64(c)))
 	for i <= j {
-		sum = i*i + j*j
+		sum := i*i + j*j
 		if sum == c {
 			return true
 		} else if sum > c {
