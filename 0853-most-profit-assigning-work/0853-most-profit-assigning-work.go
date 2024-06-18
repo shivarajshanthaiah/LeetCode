91b2ceee2927de557e0b6f1197c6bf7f7bb172d3
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-18 11:32:14 +0530

    Time: 60 ms (44%), Space: 7.2 MB (80%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.go
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.go
@@ -1,25 +1,25 @@
 func maxProfitAssignment(difficulty []int, profit []int, worker []int) int {
-    var jobs [][]int
-    for i := 0;i<len(difficulty);i++{
-        jobs = append(jobs, []int{difficulty[i], profit[i]})
-    }
-    sort.Slice(jobs, func(i, j int) bool{
-        return jobs[i][0] < jobs[j][0]
-    })
+	var jobs [][]int
+	for i := 0; i < len(difficulty); i++ {
+		jobs = append(jobs, []int{difficulty[i], profit[i]})
+	}
+	sort.Slice(jobs, func(i, j int) bool {
+		return jobs[i][0] < jobs[j][0]
+	})
 
-    sort.Ints(worker)
-    count := 0
-    maxProf := 0
-    temp := 0
+	sort.Ints(worker)
+	count := 0
+	maxProf := 0
+	temp := 0
 
-    for i := 0;i<len(worker);i++{
-        for temp < len(jobs) && jobs[temp][0] <= worker[i]{
-            if jobs[temp][1] > maxProf{
-                maxProf = jobs[temp][1]
-            }
-            temp++
-        }
-        count += maxProf
-    }
-    return count
+	for i := 0; i < len(worker); i++ {
+		for temp < len(jobs) && jobs[temp][0] <= worker[i] {
+			if jobs[temp][1] > maxProf {
+				maxProf = jobs[temp][1]
+			}
+			temp++
+		}
+		count += maxProf
+	}
+	return count
 }
