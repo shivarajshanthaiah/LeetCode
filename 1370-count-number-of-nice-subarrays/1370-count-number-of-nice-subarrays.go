cbb6dc30d4d5e49d4263b9f99a09cfaba290c51a
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-06-22 10:39:28 +0530

    Time: 143 ms (7.81%), Space: 10.4 MB (17.19%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.go
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.go
@@ -1,6 +1,5 @@
 func numberOfSubarrays(nums []int, k int) int {
-	sumFreq := make(map[int]int)
-	sumFreq[0] = 1
+	sumFreq := map[int]int{0:1}
 	sum, res := 0, 0
 	for _, num := range nums {
 		sum += num % 2
