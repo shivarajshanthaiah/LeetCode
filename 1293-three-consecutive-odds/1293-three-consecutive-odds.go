4a89c157577899c508813f1cbdce3b4e0f68097b
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-07-01 09:39:05 +0530

    Time: 2 ms (83.33%), Space: 2.8 MB (13.33%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.go
@@ -0,0 +1,14 @@
+func threeConsecutiveOdds(arr []int) bool {
+	count := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i]%2 != 0 {
+			count++
+		} else {
+			count = 0
+		}
+		if count > 2 {
+			return true
+		}
+	}
+	return false
+}
