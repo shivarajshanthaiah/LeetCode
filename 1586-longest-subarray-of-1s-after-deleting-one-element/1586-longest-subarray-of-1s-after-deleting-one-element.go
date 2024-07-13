c5b0b02126cc787d417a85d0da872c86a2427675
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-07-13 09:43:39 +0530

    Time: 60 ms (30.6%), Space: 7.5 MB (84.48%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.go
@@ -0,0 +1,17 @@
+func longestSubarray(nums []int) int {
+	maxCount, zeros := 0, 0
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] == 0 {
+			zeros++
+		}
+		for zeros > 1 {
+			if nums[left] == 0 {
+				zeros--
+			}
+			left++
+		}
+		maxCount = max(right-left, maxCount)
+	}
+	return maxCount
+}
