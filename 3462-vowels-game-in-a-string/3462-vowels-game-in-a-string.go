d042c874ae33fb7ca3b920f07217993685764e9e
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-07-24 09:51:36 +0530

    Time: 13 ms (68.04%), Space: 7.1 MB (10.31%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.go
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.go
@@ -1,9 +1,8 @@
 func doesAliceWin(s string) bool {
-	count := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' {
-			count++
+			return true
 		}
 	}
-	return count > 0
+	return false
 }
