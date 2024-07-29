ca458a54a443287299bf24e6aa53d9807a88c5d3
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-07-29 09:35:10 +0530

    Time: 1832 ms (17.39%), Space: 2.9 MB (34.78%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.go
+++ 1511-count-number-of-teams/1511-count-number-of-teams.go
@@ -1,9 +1,13 @@
 func numTeams(rating []int) int {
 	count := 0
+	fir, sec, th := 0, 0, 0
 	for i := 0; i < len(rating); i++ {
+		fir = rating[i]
 		for j := i + 1; j < len(rating); j++ {
+			sec = rating[j]
 			for k := j + 1; k < len(rating); k++ {
-				if rating[i] > rating[j] && rating[j] > rating[k] || rating[i] < rating[j] && rating[j] < rating[k] {
+				th = rating[k]
+				if fir > sec && sec > th || fir < sec && sec < th {
 					count++
 				}
 			}
