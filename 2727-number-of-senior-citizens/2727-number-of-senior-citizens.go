272aa017a7df74960aa7d10884d6c2361befa822
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-01 14:47:16 +0530

    Time: 0 ms (100%), Space: 3.1 MB (100%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.go
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.go
@@ -1,10 +1,7 @@
 func countSeniors(details []string) int {
 	res := 0
-	for _, v := range details {
-		l := len(v)
-		ageStr := string(v[l-4]) + string(v[l-3])
-		age, _ := strconv.Atoi(ageStr)
-		if age > 60 {
+	for _, d := range details {
+		if d[11] > '6' || d[11] == '6' && d[12] > '0' {
 			res++
 		}
 	}
