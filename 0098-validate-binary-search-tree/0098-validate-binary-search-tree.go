853e23a25f9d20cb73d5929c74430a8792fe16d0
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-21 09:43:19 +0530

    Time: 6 ms (51%), Space: 5.4 MB (34.93%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return isValid(root, math.MinInt, math.MaxInt)
+}
+
+func isValid(root *TreeNode, min, max int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val <= min || root.Val >= max {
+		return false
+	}
+	return isValid(root.Left, min, root.Val) && isValid(root.Right, root.Val, max)
+} 
