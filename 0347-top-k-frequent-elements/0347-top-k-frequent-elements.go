8010f7501deb0a5f12b7476a6ff405b79988a684
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-23 18:30:53 +0530

    Time: 12 ms (44.25%), Space: 6.1 MB (74.66%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -0,0 +1,18 @@
+func topKFrequent(nums []int, k int) []int {
+	freq := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		freq[nums[i]]++
+	}
+
+	unique := make([]int, 0, len(freq))
+	for num := range freq {
+		unique = append(unique, num)
+	}
+
+	sort.Slice(unique, func(i, j int) bool {
+		return freq[unique[i]] > freq[unique[j]]
+	})
+    
+	return unique[:k]
+}
