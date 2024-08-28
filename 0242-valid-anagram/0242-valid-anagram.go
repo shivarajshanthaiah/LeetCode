697f54b1906c094dfa27f08a0c270eafa4be0545
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-28 10:08:00 +0530

    Time: 7 ms (54.24%), Space: 3.1 MB (26.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -2,14 +2,21 @@ func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
+
 	map1 := make(map[rune]int)
 	map2 := make(map[rune]int)
 
-	for _, ch := range s {
-		map1[ch]++
-	}
-	for _, ch := range t {
-		map2[ch]++
+	// for _, ch := range s {
+	// 	map1[ch]++
+	// }
+
+	// for _, ch := range t {
+	// 	map2[ch]++
+	// }
+
+	for i := 0; i < len(s); i++ {
+		map1[rune(s[i])]++
+		map2[rune(t[i])]++
 	}
 
 	for key, _ := range map1 {
