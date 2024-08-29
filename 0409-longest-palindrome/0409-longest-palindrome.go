d13eb0f89dd5acbdc5eb71a668c072ddccd1a5b5
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-29 12:51:57 +0530

    Time: 0 ms (100%), Space: 2.3 MB (69.37%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.go
+++ 0409-longest-palindrome/0409-longest-palindrome.go
@@ -1,24 +1,42 @@
+// func longestPalindrome(s string) int {
+
+// 	freq := make(map[rune]int)
+// 	for _, char := range s {
+// 		freq[char]++
+// 	}
+
+// 	foundOdd := false
+// 	length := 0
+
+// 	for _, char := range freq {
+// 		if char%2 == 0 {
+// 			length += char
+// 		} else {
+// 			length += char - 1
+// 			foundOdd = true
+// 		}
+// 	}
+
+// 	if foundOdd {
+// 		length++
+// 	}
+// 	return length
+// }
+
 func longestPalindrome(s string) int {
+	freq := make(map[rune]bool)
+	res := 0
 
-	freq := make(map[rune]int)
-	for _, char := range s {
-		freq[char]++
-	}
-
-	foundOdd := false
-	length := 0
-
-	for _, char := range freq {
-		if char%2 == 0 {
-			length += char
+	for _, c := range s {
+		if freq[c] {
+			delete(freq, c)
+			res += 2
 		} else {
-			length += char - 1
-			foundOdd = true
+			freq[c] = true
 		}
 	}
-
-	if foundOdd {
-		length++
+	if len(freq) > 0 {
+		return res + 1
 	}
-	return length
+	return res
 }
