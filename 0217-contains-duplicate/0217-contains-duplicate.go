818534b8430083fa4a47f8747d150f754c42f6aa
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-08-29 14:45:54 +0530

    Time: 75 ms (74.38%), Space: 10 MB (48.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -2,10 +2,7 @@ func containsDuplicate(nums []int) bool {
 	dup := make(map[int]int, len(nums))
 	for _, v := range nums {
 		dup[v]++
-	}
-
-	for _, v := range dup {
-		if v > 1 {
+		if dup[v] > 1 {
 			return true
 		}
 	}
