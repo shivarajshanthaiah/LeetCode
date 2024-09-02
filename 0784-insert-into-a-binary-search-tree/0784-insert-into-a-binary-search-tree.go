aa8c2f470baea10fa67f2a3c99be452168fd949d
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-09-02 14:48:37 +0530

    Time: 21 ms (78%), Space: 7.3 MB (51.5%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.go
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.go
@@ -10,10 +10,9 @@ func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	if root == nil {
 		return &TreeNode{Val: val}
 	}
-	if val > root.Val {
+	if root.Val < val {
 		root.Right = insertIntoBST(root.Right, val)
-	}
-	if val < root.Val {
+	} else if root.Val > val {
 		root.Left = insertIntoBST(root.Left, val)
 	}
 	return root
