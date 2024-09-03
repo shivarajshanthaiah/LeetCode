8c29ac51daf7289f072a089a274cf6f434d61e81
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-09-03 12:27:11 +0530

    Time: 8 ms (53.83%), Space: 6 MB (13.85%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.go
+++ 2058-concatenation-of-array/2058-concatenation-of-array.go
@@ -1,3 +1,7 @@
 func getConcatenation(nums []int) []int {
-	return append(nums, nums...)
+	// return append(nums, nums...)
+	for _, num := range nums {
+		nums = append(nums, num)
+	}
+	return nums
 }
