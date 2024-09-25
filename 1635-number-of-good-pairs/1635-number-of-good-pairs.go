6b77c2e12d48a2eb2ace7d8eb3a87595920da75f
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-09-25 10:57:21 +0530

    Time: 1 ms (80.45%), Space: 2.1 MB (96.09%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.go
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.go
@@ -1,7 +1,7 @@
 func numIdenticalPairs(nums []int) int {
 	pairs := 0
 	for i := 0; i < len(nums); i++ {
-		for j := i; j < len(nums); j++ {
+		for j := i+1; j < len(nums); j++ {
 			if i < j && nums[i] == nums[j] {
 				pairs++
 			}
