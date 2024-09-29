1a1dcb77b0dc63759d97c908651342c10c867090
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-09-29 11:06:08 +0530

    Time: 1 ms (77.78%), Space: 2.2 MB (80%) - LeetHub

--- 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.go
+++ 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.go
@@ -2,22 +2,16 @@ func removePalindromeSub(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-	if isPal(s) {
-		return 1
-	}
-	return 2
-}
-
-func isPal(s string) bool {
+    
 	i := 0
 	j := len(s) - 1
 
 	for i < j {
 		if s[i] != s[j] {
-			return false
+			return 2
 		}
 		i++
 		j--
 	}
-	return true
+	return 1
 }
