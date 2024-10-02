d8b2d9dfa288a4bfb73cf38bba280dae845c360f
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-10-02 22:34:05 +0530

    Time: 78 ms (72.09%), Space: 7.9 MB (67.44%) - LeetHub

---
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getAllElements(root1 *TreeNode, root2 *TreeNode) []int {
+	arr := []int{}
+
+	inOrder(root1, &arr)
+	inOrder(root2, &arr)
+
+	sort.Ints(arr)
+	return arr
+}
+
+func inOrder(root *TreeNode, arr *[]int) {
+	if root == nil {
+		return
+	}
+	inOrder(root.Left, arr)
+	*arr = append(*arr, root.Val)
+	inOrder(root.Right, arr)
+}
