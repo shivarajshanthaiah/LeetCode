6ac5615483901f29c061e823e198a31158968bd9
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-10-04 12:50:52 +0530

    Time: 60 ms (62.86%), Space: 7.9 MB (65.71%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.go
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.go
@@ -1,12 +1,12 @@
 func dividePlayers(skill []int) int64 {
 
-    sort.Ints(skill)
-    n := len(skill)
+	sort.Ints(skill)
+	n := len(skill)
 	target := skill[0] + skill[n-1]
 
 	ans := 0
 	for i := 0; i < n/2; i++ {
-		if skill[i]+skill[n-1 -i] != target {
+		if skill[i]+skill[n-1-i] != target {
 			return -1
 		} else {
 			ans += skill[i] * skill[n-1-i]
