f871aa3c4278e15418ddb5b6d683d75b5fa90b9c
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-10-10 22:17:49 +0530

    Time: 44 ms (47.37%), Space: 8.5 MB (21.05%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.go
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.go
@@ -1,15 +1,14 @@
 func maxWidthRamp(nums []int) int {
 	stack := []int{}
-	n := len(nums)
 	ans := 0
 
-	for i := 0; i < n; i++ {
+	for i := 0; i < len(nums); i++ {
 		if len(stack) == 0 || nums[stack[len(stack)-1]] > nums[i] {
 			stack = append(stack, i)
 		}
 	}
 
-	for j := n - 1; j >= 0; j-- {
+	for j := len(nums) - 1; j >= 0; j-- {
 		for len(stack) > 0 && nums[stack[len(stack)-1]] <= nums[j] {
 			ans = max(ans, j-stack[len(stack)-1])
 			stack = stack[:len(stack)-1]
