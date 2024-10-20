184caf44ede611a39296f9b1198108eda05bf6bb
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-10-20 21:47:28 +0530

    Time: 24 ms (83.33%), Space: 8.5 MB (6.06%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	tempA, tempB := headA, headB
+	for tempA != tempB {
+		if tempA == nil {
+			tempA = headB
+		} else {
+			tempA = tempA.Next
+		}
+
+		if tempB == nil {
+			tempB = headA
+		} else {
+			tempB = tempB.Next
+		}
+	}
+	return tempA
+}
