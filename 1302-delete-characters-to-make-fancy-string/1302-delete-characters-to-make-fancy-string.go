e09eda098a809dcd53f6268ff39379969f03d478
Shivaraj S Shanthaiah <155908491+shivarajshanthaiah@users.noreply.github.com>
2024-11-03 17:29:28 +0530

    Time: 14 ms (44.79%), Space: 9.4 MB (51.4%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.go
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.go
@@ -1,4 +1,7 @@
 func makeFancyString(s string) string {
+    if len(s) < 3{
+        return s
+    }
 	ans := []byte{}
 	for i := 0; i < len(s); i++ {
 		if i < len(s)-2 && s[i] == s[i+1] && s[i+1] == s[i+2] {
